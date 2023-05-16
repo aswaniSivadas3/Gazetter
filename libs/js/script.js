fe56e63450d87a25b383b73d3dbc8b835bf41c26
aswaniSivadas3 <email.aswanisivadas@gmail.com>
2023-05-16 16:02:34 +0100

    gazatter-script

--- libs/js/script.js
+++ libs/js/script.js
@@ -12,6 +12,7 @@ var countryCurrencyName;
 var wikiUrl;
 
 
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 
@@ -26,15 +27,15 @@ var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OPNVKarte = L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 var baseMaps={
     'Open Street Map': OSM,
     'Open TopoMap':OpenTopoMap,
-    'Stadia AlidadeSmoothDark':Stadia_AlidadeSmoothDark
+    'OPNVKarte':OPNVKarte
  }
 
     if(!navigator.geolocation){
@@ -62,6 +63,7 @@ var baseMaps={
 
 $('document').ready(function() {
     
+
     countryLat=userLat;
     countryLng=userLng
 
@@ -487,7 +489,3 @@ wikiEasy = new easyButton('Wiki','&#x1F4D6');
 timeZone = new easyButton('Time','&#x1F570');
 flag = new easyButton('Flag','&#127988');
 
-function linkUrlToWiki()
-{
-    location.href=wikiUrl;
-}
